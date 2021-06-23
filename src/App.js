c07155699164b6aa98058a509d64bdba7b102a76
rohitsharma0027 <developer.rohitsharmaj7@gmail.com>
2021-06-23 07:43:03 +0530

    Added the dynamic routing and extraced the route params

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import {Route} from 'react-router-dom';
 import Welcome from './components/Welcome';
 import Products from './components/Products'
 import MainHeader from './components/MainHeader';
+import ProductDetail from './components/ProductDetail';
 import './App.css';
 
 function App() {
@@ -10,14 +11,17 @@ function App() {
     <MainHeader/>
     <div>
       <Route path="/" exact>
-        <h2>Lets' get started</h2>
+        <h2>Lets' get started</h2>  
       </Route>
       <Route path="/welcome">
          <Welcome/>  
       </Route>
       <Route path="/products">
          <Products/>  
-      </Route>  
+      </Route> 
+      <Route path="/productdetails/:productId">
+         <ProductDetail/>  
+      </Route>   
     </div>
     </>
   );
